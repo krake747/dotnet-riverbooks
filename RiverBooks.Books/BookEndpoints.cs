f96a715891a57b8488585eb279d8aeb94ede7dd6
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-03-09 17:23:36 +0100

    feat(riverbooks): Add Book Entity

--- RiverBooks.Books/BookEndpoints.cs
+++ RiverBooks.Books/BookEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using FastEndpoints;
+﻿using Ardalis.GuardClauses;
+using FastEndpoints;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
@@ -22,6 +23,28 @@ internal sealed class BookService : IBookService
 
 public sealed record BookDto(Guid Id, string Title, string Author);
 
+internal sealed class Book
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public decimal Price { get; private set; }
+
+    internal Book(Guid id, string title, string author, decimal price)
+    {
+        Id = Guard.Against.Default(id);
+        Title = Guard.Against.NullOrEmpty(title);
+        Author = Guard.Against.NullOrEmpty(author);
+        Price = Guard.Against.Negative(price);
+    }
+
+    internal decimal UpdatePrice(decimal newPrice)
+    {
+        Price = Guard.Against.Negative(newPrice);
+        return newPrice;
+    }
+}
+
 public static class BookEndpoints
 {
     public const string GetAll = "/books";
@@ -36,7 +59,7 @@ public static class BookEndpoints
 
 public sealed class ListBooksResponse
 {
-    public IEnumerable<BookDto> Books { get; set; }
+    public IEnumerable<BookDto> Books { get; set; } = [];
 }
 
 internal class ListBooksEndpoint(IBookService bookService) 
