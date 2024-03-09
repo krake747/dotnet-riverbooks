7cdd51197b9265d691dacb47bcd279d327d77067
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-03-09 22:11:42 +0100

    feat(riverbooks): Set up infra and application

---
+++ README.md
@@ -0,0 +1,12 @@
+﻿# RiverBooks
+
+## EntityFramework
+
+```bash
+cd RiverBooks.Api
+dotnet tool install -g dotnet-ef
+dotnet tool update -g dotnet-ef
+dotnet ef migrations add Initial -c BookDbContext -p ..\RiverBooks.Books\RiverBooks.Books.csproj -s .\RiverBooks.Api.csproj -o Data/Migrations
+dotnet ef database update
+```
+
--- RiverBooks.Books/BookEndpoints.cs
+++ RiverBooks.Books/BookEndpoints.cs
@@ -1,15 +1,24 @@
-﻿using Ardalis.GuardClauses;
+﻿using System.Reflection;
+using Ardalis.GuardClauses;
 using FastEndpoints;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RiverBooks.Books;
 
 public static class BookServiceCollectionExtensions
 {
-    public static IServiceCollection AddBookServices(this IServiceCollection services)
+    public static IServiceCollection AddBookServices(this IServiceCollection services, ConfigurationManager config)
     {
+        var connectionString = config.GetConnectionString("Books");
+
+        services.AddDbContext<BookDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddScoped<IBookRepository, EfBookRepository>();
+        
         services.AddScoped<IBookService, BookService>();
 
         return services;
@@ -66,12 +75,6 @@ internal sealed class BookService(IBookRepository bookRepository) : IBookService
     {
         var books = (await bookRepository.ListAsync())
             .Select(book => new BookDto(book.Id, book.Title, book.Author, book.Price));
-
-        // [
-        //     new BookDto(Guid.NewGuid(), "The Fellowship of the Ring", "J.R.R. Tolkien"),
-        //     new BookDto(Guid.NewGuid(), "The Two Towers", "J.R.R. Tolkien"),
-        //     new BookDto(Guid.NewGuid(), "The Return of the Ring", "J.R.R. Tolkien")
-        // ];
         
         return books;
     }
@@ -118,6 +121,30 @@ internal sealed class BookService(IBookRepository bookRepository) : IBookService
 
 public sealed record BookDto(Guid Id, string Title, string Author, decimal Price);
 
+internal sealed class EfBookRepository(BookDbContext dbContext) : IBookRepository
+{
+    public async Task<Book?> GetByIdAsync(Guid id) => 
+        await dbContext.Books.FindAsync(id);
+
+    public async Task<IEnumerable<Book>> ListAsync() =>
+        await dbContext.Books.ToListAsync();
+
+    public Task AddAsync(Book book)
+    {
+        _ = dbContext.Add(book);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Book book)     
+    {
+        _ = dbContext.Remove(book);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync() => 
+        await dbContext.SaveChangesAsync();
+}
+
 internal interface IBookRepository : IReadOnlyBookRepository
 {
     Task AddAsync(Book book);
@@ -129,6 +156,59 @@ internal interface IReadOnlyBookRepository
 {
     Task<Book?> GetByIdAsync(Guid id);
     Task<IEnumerable<Book>> ListAsync();
+}
+
+internal sealed class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    internal static readonly Guid Book1Guid = new ("A89F6CD7-4693-457B-9009-02205DBBFE45");
+    internal static readonly Guid Book2Guid = new ("E4FA19BF-6981-4E50-A542-7C9B26E9EC31");
+    internal static readonly Guid Book3Guid = new ("17C61E41-3953-42CD-8F88-D3F698869B35");
+    
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.Property(p => p.Title)
+            .HasMaxLength(DataSchemaConstants.DefaultNameLength)
+            .IsRequired();
+
+        builder.Property(p => p.Author)
+            .HasMaxLength(DataSchemaConstants.DefaultNameLength)
+            .IsRequired();
+
+        builder.HasData(GetSampleBookData());
+    }
+    
+    private static IEnumerable<Book> GetSampleBookData()
+    {
+        const string tolkien = "J.R.R. Tolkien";
+        yield return new Book(Book1Guid, "The Fellowship of the Ring", tolkien, 10.99m);
+        yield return new Book(Book2Guid, "The Two Towers", tolkien, 11.99m);
+        yield return new Book(Book3Guid, "The Return of the Ring", tolkien, 12.99m);
+    } 
+}
+
+internal sealed class BookDbContext(DbContextOptions<BookDbContext> options) : DbContext(options)
+{
+    internal DbSet<Book> Books { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("Books");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<decimal>()
+            .HavePrecision(18, 6);
+    }
+}
+
+internal static class DataSchemaConstants
+{
+    public const int DefaultNameLength = 100;
 }
 
 
